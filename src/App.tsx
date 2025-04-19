97987351c02c828751c1fe047ab2840f5da5b60a
ashokn-24 <ashokn2410@gmail.com>
2025-04-19 19:36:47 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <main>
+      <div className="">
+        <h1>Welcome to my Porfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
